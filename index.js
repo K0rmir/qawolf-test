3b360ac2eec2e02381f24103f5796c8d77cb4aa2
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-04-11 19:49:51 +0100

    add function to get top 10 article titles & urls

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
 const { chromium } = require("playwright");
 
@@ -7,9 +9,38 @@ async function saveHackerNewsArticles() {
   const context = await browser.newContext();
   const page = await context.newPage();
 
+  const articlesArr = [];
+
+
   // go to Hacker News
-  await page.goto("https://news.ycombinator.com");
+  console.log("Navigating to page...")
+  await page.goto("https://news.ycombinator.com", {timeout: 2 * 60 * 1000});
+  console.log("Landed! Getting article titles...")
+
+  // get element of article title names //
+  const articles = page.locator('tr.athing')
+
+   for (let i=0; i < 10; i++) {
+    const articleElement = articles.nth(i);
+    const titleElement = articleElement.locator('td.title span.titleline a').nth(0)
+    const title = await titleElement.textContent();
+    console.log(title)
+    const url = await titleElement.getAttribute('href');
+    console.log(url)
+    const article = {
+      Title: title,
+      Url: url
+    }
+    articlesArr.push(article)
+    console.log(articles)
+  } 
+
+
+  console.log(articlesArr)
+  await browser.close();
 }
+
+
 
 (async () => {
   await saveHackerNewsArticles();
