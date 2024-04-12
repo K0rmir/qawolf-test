f403e767388c5734e3e127b69487bc3ed7c78e97
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-04-12 12:53:47 +0100

    add function to export csv file of articles

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-
-
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
 const { chromium } = require("playwright");
-
-let converter = require('json-2-csv');
+const converter = require('json-2-csv');
+const fs = require("node:fs");
 
 
 async function saveHackerNewsArticles() {
@@ -17,7 +15,7 @@ async function saveHackerNewsArticles() {
   // go to Hacker News
   console.log("Navigating to page...")
   await page.goto("https://news.ycombinator.com", {timeout: 2 * 60 * 1000});
-  console.log("Landed! Getting article titles...")
+  console.log("Landed on Hacker News! Getting top 10 articles...")
 
   // get element where each article is contained in table row //
   const articles = page.locator('tr.athing')
@@ -37,12 +35,24 @@ async function saveHackerNewsArticles() {
   // ^ As noted here (https://playwright.dev/docs/other-locators), it is recommended to use user-visible locators like text or accessible role instead of CSS classes. //
   // However, given the structure of the HTML on the hacker news website, it was difficult to use getByRole as it mostly uses table data & rows and span elements of which are invalid arguments. //
   // I experimented with arguments such as 'table' and 'row', however didn't have luck with these. I'd welcome refactoring this in the future should I be exposed to more efficient way however I'm happy with this implementation for now. //
-  console.log(articlesArr)
-  await browser.close();
 
+  await browser.close();
+  console.log("Articles got! Compiling and downloading...")
+  // Convert articles array into csv //
   const articlesCsv = converter.json2csv(articlesArr)
 
-  console.log(articlesCsv)
+
+  function downloadArticleCsv() {
+    fs.writeFile('articles.csv', articlesCsv, err => {
+      if (err) {
+        console.log("!Failed to download articles!")
+      } else {
+        console.log("Articles download successfully!")
+      }
+    })   
+  }
+
+  downloadArticleCsv();
 
 
 }
