217dc6346d2c59810604cee439f1eef3a24b612a
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-04-15 17:07:05 +0100

    add error handling & env variables

--- index.js
+++ index.js
@@ -4,17 +4,12 @@ const converter = require('json-2-csv');
 const fs = require("node:fs");
 const nodemailer = require("nodemailer");
 const cron = require("node-cron");
-
-async function installChromium() {
-  await chromium.install();
-  await saveHackerNewsArticles();
-}
-
-installChromium();
+require('dotenv').config();
+require("process");
 
 async function saveHackerNewsArticles() {
-  // launch browser
-  const browser = await chromium.launch({ headless: false });
+  // launch browser  
+  const browser = await chromium.launch({ headless: true });
   const context = await browser.newContext();
   const page = await context.newPage();
 // empty array that article data will be pushed into //
@@ -22,38 +17,49 @@ async function saveHackerNewsArticles() {
 
   // go to Hacker News
   console.log("Navigating to page...")
-  await page.goto("https://news.ycombinator.com", {timeout: 2 * 60 * 1000});
-  console.log("Landed on Hacker News! Getting top 10 articles...")
+  try {
+    await page.goto("https://news.ycombinator.com", {timeout: 2 * 60 * 1000});
+  } catch(e) {
+    console.error(e)
+    throw new Error("Failed to establish connection.")
+  }
+ 
+  console.log("Connected to Hacker News! Getting top 10 articles...")
 
   // get element where each article is contained in table row //
   const articles = page.locator('tr.athing')
 
-   for (let i=0; i <= 9; i++) {
-    const articleElement = articles.nth(i);
-    const titleElement = articleElement.locator('td.title span.titleline a').nth(0)
-    const title = await titleElement.textContent();   
-    const url = await titleElement.getAttribute('href');   
-    const article = {
-      Title: title,
-      Url: url
-    }
-    articlesArr.push(article)
-  } 
+  try {
+    for (let i=0; i <= 9; i++) {
+      const articleElement = articles.nth(i);    
+      const titleElement = articleElement.locator('td.title span.titleline a').nth(0)
+      const title = await titleElement.textContent();   
+      const url = await titleElement.getAttribute('href'); 
+      const article = {
+        Title: title,
+        Url: url
+      }
+      articlesArr.push(article)
+  }} catch(err) {
+    console.error(err)
+    throw new Error("Failed to scrape article data. Aborting...")
+  }  
 
   // ^ As noted here (https://playwright.dev/docs/other-locators), it is recommended to use user-visible locators like text or accessible role instead of CSS classes. //
   // However, given the structure of the HTML on the hacker news website, it was difficult to use getByRole as it mostly uses table data & rows and span elements, which are invalid arguments. //
   // I experimented with arguments such as 'table' and 'row', however didn't have luck with these. I'd welcome refactoring this in the future should I be exposed to a more efficient way however I'm happy with this implementation for now. //
 
   await browser.close();
+
+    // Convert articles array into csv //
   console.log("Articles got! Converting and downloading...")
-  // Convert articles array into csv //
   const articlesCsv = converter.json2csv(articlesArr)
 
 // Create csv file //
   function downloadArticleCsv() {
     fs.writeFile('articles.csv', articlesCsv, err => {
       if (err) {
-        console.log("!Failed to download articles!")
+        console.error("Failed to download articles!")
       } else {
         console.log("Articles download successfully!")
       }
@@ -76,13 +82,13 @@ async function saveHackerNewsArticles() {
       port: 465,
       secure: true,
       auth: {
-        user: 'kormir.dev@gmail.com',
-        pass: 'itiv uijk sjsw stpo'
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS
       }
     });
 // function to send email //
     const message = await transporter.sendMail({
-      from: 'Kormir <kormir.dev@gmail.com',
+      from: `Kormir <kormir.dev@gmail.com>`,
       to: 'kormir.dev@gmail.com',
       subject: 'Hacker News Daily',
       html: html,
@@ -95,17 +101,20 @@ async function saveHackerNewsArticles() {
   }
   
   sendArticles()
-
 }
+
+(async () => {
+  await saveHackerNewsArticles();
+})();
 
 // Automated scheduleing to recieve emails each day //
 
-cron.schedule('*/16 * * * *', () => {
-  (async () => {
-    await saveHackerNewsArticles();
-  })();
-  console.log("CronJob Ran Successfully!")    
-});
+// cron.schedule('*/5 * * * *', () => {
+//   (async () => {
+//     await saveHackerNewsArticles();
+//   })();
+//   console.log("CronJob Ran Successfully!")    
+// });
 
 
 
